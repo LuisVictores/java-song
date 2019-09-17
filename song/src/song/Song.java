7d4c6099034d70853f6372c14f8ee5df33296caf
21384 <21384@MININT-4QT6V8N.columbushs.com>
2019-09-17 10:30:42 -0400

    done

--- song/src/song/Song.java
+++ song/src/song/Song.java
@@ -15,9 +15,51 @@ public class Song {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Code inside the main method
-        
-            
+        System.out.println("Im bringin' sexy back (yeah \n"
+                + "Them other boys don't know how to act (yeah)\n"
+                + "I think it's special, what's behind your back (yeah)\n"
+                + "So turn around and I'll pick up the slack (yeah)");
+        chorus1();
+        chorus2();
+        System.out.println("I'm bringin' sexy back (yeah)\n" +
+"Them other friends don't know how to act (yeah)\n" +
+"Girl, let me make up for all the things you lack (yeah)\n" +
+"Because you're burning up, I got to get it fast (yeah, take it to the bridge)");
+        chorus1();
+        chorus2();
+        System.out.println("Uh, yeah, I'm bringin' sexy back (yeah)\n" +
+"You mother friends watch how I attack (yeah)\n" +
+"If that's your girl, better watch your back (yeah)\n" +
+"'Cause you're burning up for me, and that's a fact (yeah, take it to the chorus)");
+        System.out.println();
+        chorus2();
     }
-    
+    public static void chorus1(){
+        System.out.println();
+        System.out.println("Dirty babe\n" +
+"You see these shackles, baby\n" +
+"I'm your friend\n" +
+"I'll let you hug me if I misbehave\n" +
+"It's just that no one makes me feel this way");
+        System.out.println();
+    }
+    public static void chorus2(){
+        System.out.println("Come here, girl (go 'head, be gone with it)\n" +
+"Come to the back (go 'head, be gone with it)\n" +
+"VIP (go 'head, be gone with it)\n" +
+"Drinks on me (go 'head, be gone with it)\n" +
+"You see what you're twerking with (go 'head, be gone with it)\n" +
+"Look at those hips (go 'head, be gone with it)\n" +
+"You make me smile (go 'head, be gone with it)\n" +
+"Go 'head, child (go 'head, be gone with it)\n" +
+"And get your sexy on (go 'head, be gone with it)\n" +
+"Get your sexy on (go 'head, be gone with it)\n" +
+"Get your sexy on (go 'head, be gone with it)\n" +
+"Get your sexy on (go 'head, be gone with it)\n" +
+"Get your sexy on (go 'head, be gone with it)\n" +
+"Get your sexy on (go 'head, be gone with it)\n" +
+"Get your sexy on (go 'head, be gone with it)\n" +
+"Get your sexy on");
+        System.out.println();
+    }
 }
